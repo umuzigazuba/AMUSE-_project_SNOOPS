b0107e14276ce45331224fef3a4236bf8749037f
umuzigazuba <erin.umuzigazuba@gmail.com>
2023-11-22 17:21:02 +0100

    adjusted plot_collision function to subplots

--- AMUSE_project_YESSIR/notebooks/collisiontest.py
+++ AMUSE_project_YESSIR/notebooks/collisiontest.py
@@ -18,35 +18,33 @@ from amuse.units import units
 from amuse.community.bhtree.interface import Bhtree
 #%%
 
-particles_cloud, converter_cloud  = make_molecular_cloud(N_cloud = 50_000,
+particles_cloud, converter_cloud  = make_molecular_cloud(N_cloud = 1_00,
                                                          M_cloud = 1_000 | units.MSun,
-                                                         R_cloud = 20 | units.parsec,
+                                                         R_cloud = 10 | units.parsec,
                                                          seed = 1312)
-particles_cloud = evolve_molecular_cloud(particles_cloud, 
+
+particles_cloud, colorbar = evolve_molecular_cloud(particles_cloud, 
                                          converter_cloud, 
-                                         2 | units.Myr, 
-                                         0.2 | units.Myr, 
-                                         1 | units.RSun,
-                                         1312)
+                                         t_end = 2 | units.Myr, 
+                                         dt = 0.2 | units.Myr, 
+                                         seed = 1312)
 
 
 #%%
 collision_bodies = Particles(1)
 collision_bodies[0].name = "star"
-collision_bodies[0].mass = 25 |units.MSun
-collision_bodies[0].radius = 8 | units.RSun
-collision_bodies[0].position = (-1.0,0,0) * (45 | units.pc)
-collision_bodies[0].velocity = (1.0,0,0) * (60 | units.kms)
+collision_bodies[0].mass = 150 |units.MSun
+collision_bodies[0].radius = 35 | units.RSun
+collision_bodies[0].position = (-1.0,0,0) * (20 | units.pc)
+collision_bodies[0].velocity = (1.0,0,0) * (20 | units.kms)
 converter_star=nbody_system.nbody_to_si(collision_bodies[0].mass.sum(), 
                                    collision_bodies[0].position.length())
 
 gravity_code = Bhtree(converter_star)
 gravity_code.particles.add_particles(collision_bodies)
-# ch_gravity2star = gravity_code.particles.new_channel_to(collision_bodies)
 
 #%%
-resolution = 1 | units.RSun
-timestep = 0.2 | units.Myr
+timestep = 0.1 | units.Myr
 
 np.random.seed(1312)
 
@@ -61,12 +59,6 @@ hydro_code.parameters.gamma = 1 # gas polytropic index (1.6666667) (default valu
 hydro_code.parameters.isothermal_flag = True # Isothermal flag. True means: isothermal gas (requires integrate_entropy_flag == False). (default value:False)
 hydro_code.parameters.integrate_entropy_flag = False # Integrate-entropy flag. True means: integrate entropy, else: internal energy. (default value:True)
 hydro_code.parameters.timestep = 0.5*timestep # timestep for system (default value:4.70451599238e+13 s)
-
-hydro_code.parameters.eps_is_h_flag = False  #Eps-is-h flag. True means: set gas particles gravitational epsilon to h (SPH smoothing length). (default value:True)
-                                            # h_smooth is constant
-hydro_code.parameters.gas_epsilon = resolution # The gas gravitational smoothing epsilon.
-hydro_code.parameters.sph_h_const = resolution # SPH smoothing length if constant
-particles_cloud.h_smooth= resolution
 
 hydro_code.gas_particles.add_particles(particles_cloud)
 
@@ -92,72 +84,88 @@ gravhydrobridge.add_system(hydro_code, (gravity_code,) )
 gravhydrobridge.timestep = 0.2|units.Myr
 #%%
 
-def plot_cloud_star(time, hydro_code, star_particle, L,N):
-    fig = plt.figure(figsize = (9, 5))
- 
-    rho = make_map(hydro_code, L = L, N = N)
-    cax = plt.imshow(np.log10(rho.value_in(units.amu/units.cm**3)), extent=[-L,L,-L,L]) # , vmin = 0, vmax = 5
-    plt.scatter(star_particle.x.value_in(units.pc), star_particle.y.value_in(units.pc), c = 'red')
-    cbar = fig.colorbar(cax)
-    cbar.set_label('log density [$amu/cm^3$]', labelpad = 5)
-
-    plt.title("Molecular cloud at time = " + time.as_string_in(units.Myr))
-    plt.xlabel("x [pc]")
-    plt.ylabel("y [pc]")
-    plt.show()
-
-
-def zoomed_in_star(time, hydro_code, star_particle, L, x_center,y_center,N):
-    fig = plt.figure(figsize = (9, 5))
- 
-    rho = make_map(hydro_code, L = L, N = N)
-    cax = plt.imshow(np.log10(rho.value_in(units.amu/units.cm**3)), extent=[-L,L,-L,L]) # , vmin = 0, vmax = 5
+def plot_cloud_star(time, hydro_code, star_particle, L, x_center, y_center, N, colorbar):
+    rho = make_map(hydro_code, L=L, N=N)
     
-    cbar = fig.colorbar(cax)
-    cbar.set_label('log density [$amu/cm^3$]', labelpad = 5)
+    fig, (ax_full, ax_zoom) = plt.subplots(nrows = 2, ncols = 1, figsize=(10.5, 7))
+ 
+    im_full = ax_full.imshow(np.log10(rho.value_in(units.amu/units.cm**3)), extent=[-L, L, -L, L])
+    ax_full.scatter(star_particle.x.value_in(units.pc), star_particle.y.value_in(units.pc), c='red')
+
+    ax_full.set_title("Molecular cloud at time = " + time.as_string_in(units.Myr))
+    ax_full.set_xlabel("x [pc]")
+    ax_full.set_ylabel("y [pc]")
+
+    im_zoom = ax_zoom.imshow(np.log10(rho.value_in(units.amu/units.cm**3)), extent=[-L, L, -L, L])
+    ax_zoom.scatter(star_particle.x.value_in(units.pc), star_particle.y.value_in(units.pc), c='red')
 
     offset = L/10 + 5
 
-    plt.axis([x_center-offset, x_center+offset, y_center-5, y_center+5])
-    plt.scatter(star_particle.x.value_in(units.pc), star_particle.y.value_in(units.pc), c = 'red')
-    plt.title("Zoomed in molecular cloud at time = " + time.as_string_in(units.Myr))
-    plt.xlabel("x [pc]")
-    plt.ylabel("y [pc]")
+    ax_zoom.axis([x_center - offset, x_center + offset, y_center - 5, y_center + 5])
+    ax_zoom.set_title("Zoomed in molecular cloud at time = " + time.as_string_in(units.Myr))
+    ax_zoom.set_xlabel("x [pc]")
+    ax_zoom.set_ylabel("y [pc]")
+    
+    cbar_ax = fig.add_axes([0.75, 0.1, 0.02, 0.85])
+    cbar = fig.colorbar(colorbar, cax = cbar_ax)
+    cbar.set_label('log density [$amu/cm^3$]', labelpad = 5)
+
+    plt.tight_layout()
     plt.show()
+
 #%%
 
 model_time = 0 | units.Myr
 dt = timestep
-t_end = 1.4 | units.Myr
+t_end = 2 | units.Myr
 L = int(max(particles_cloud.x.value_in(units.pc))) 
 L = L + L
+
+star_x_position = []
+star_y_position = []
+star_z_position = []
 
 while model_time < t_end:
 
     model_time += dt
-
+    model_time = model_time.round(2)
 
     gravhydrobridge.evolve_model(model_time)
     channel["to_star"].copy()
     channel["to_cloud"].copy()
     channel["hydro_to_star"].copy()
+    
     print('Collision in progress at ',model_time.value_in(units.Myr), 'Myr')
-    print("star position", star.position.value_in(units.pc), "pc")
-    plot_cloud_star(model_time, hydro_code, collision_bodies[0], L,1000)
 
     star_x= star.position[0][0].value_in(units.pc)
     star_y= star.position[0][1].value_in(units.pc)
+    star_z= star.position[0][2].value_in(units.pc)
 
-    zoomed_in_star(model_time, hydro_code, collision_bodies[0], L, star_x,star_y,1000)
+    star_x_position.append(star_x)
+    star_y_position.append(star_y)
+    star_z_position.append(star_z)
+
+    plot_cloud_star(model_time, hydro_code, collision_bodies[0], L, star_x, star_y, 500, colorbar)
 
 gravity_code.stop()
 hydro_code.stop()
 
+# %%
+plt.scatter(star_x_position, star_y_position)
+plt.title(f"(x, y)-position of star over {t_end.value_in(units.Myr)} Myr.")
+plt.xlabel("x [pc]")
+plt.ylabel("y [pc]")
+plt.show()
 
+plt.scatter(star_x_position, star_z_position)
+plt.title(f"(x, z)-position of star over {t_end.value_in(units.Myr)} Myr.")
+plt.xlabel("x [pc]")
+plt.ylabel("z [pc]")
+plt.show()
 
 # %%
-print("star position", star.position[0][1].value_in(units.pc))
-print("star mass", star.mass)
-# %%
-plot_cloud_star(t_end, hydro_code, collision_bodies[0], 5, 1000)
-# %%
+# print("star position", star.position[0][1].value_in(units.pc))
+# print("star mass", star.mass)
+# # %%
+# plot_cloud_star(t_end, hydro_code, collision_bodies[0], 5, 1000)
+# # %%
--- AMUSE_project_YESSIR/src/molecular_cloud_initialization.py
+++ AMUSE_project_YESSIR/src/molecular_cloud_initialization.py
@@ -1,3 +1,4 @@
+# %%
 from amuse.lab import nbody_system
 
 from amuse.ext.molecular_cloud import molecular_cloud
@@ -46,13 +47,25 @@ def plot_hydro(time, hydro, L, N):
     fig = plt.figure(figsize = (9, 5))
     
     rho = make_map(hydro, L = L, N = N)
-    cax = plt.imshow(np.log10(rho.value_in(units.amu/units.cm**3)), extent=[-L, L, -L, L]) # , vmin = 0, vmax = 5
+    cax = plt.imshow(np.log10(rho.value_in(units.amu/units.cm**3)), extent=[-L, L, -L, L])
     cbar = fig.colorbar(cax)
     cbar.set_label('log density [$amu/cm^3$]', labelpad = 5)
         
     plt.title("Molecular cloud at time = " + time.as_string_in(units.Myr))
     plt.xlabel("x [pc]")
-    plt.ylabel("x [pc]")
+    plt.ylabel("y [pc]")
+    plt.show()
+
+    return cax
+
+def plot_cloud_particles(time, particles_cloud):
+
+    fig = plt.figure(figsize = (9, 5))
+
+    plt.scatter(particles_cloud.x.value_in(units.pc), particles_cloud.y.value_in(units.pc), s = 1)
+    plt.title("Molecular cloud at time = " + time.as_string_in(units.Myr))
+    plt.xlabel("x [pc]")
+    plt.ylabel("y [pc]")
     plt.show()
 
 # %%
@@ -88,7 +101,7 @@ def make_molecular_cloud(N_cloud, M_cloud, R_cloud, seed):
 
 # %%
 
-def evolve_molecular_cloud(particles_cloud, converter_cloud, t_end, dt, resolution, seed):
+def evolve_molecular_cloud(particles_cloud, converter_cloud, t_end, dt, seed):
     '''
     Description:
         Evolve a existing molecular cloud to a certain age
@@ -109,23 +122,17 @@ def evolve_molecular_cloud(particles_cloud, converter_cloud, t_end, dt, resoluti
     np.random.seed(seed)
 
     hydro_cloud = Fi(converter_cloud)
+    hydro_cloud.gas_particles.add_particles(particles_cloud)
 
     hydro_cloud.parameters.use_hydro_flag = True # Hydrodynamics flag. True means: SPH hydro included, False means: gravity only.
-    hydro_cloud.parameters.radiation_flag = False # Radiation flag. True means: radiation (i.e. radiative cooling/heating) is included. False means: no radiation, and implies no star formation. (default value:False)
+    hydro_cloud.parameters.radiation_flag = False # Radiation flag. True means: 
+                                                #radiation (i.e. radiative cooling/heating) is included. 
+    hydro_cloud.parameters.timestep = dt # timestep for system (default value:4.70451599238e+13 s)
 
     hydro_cloud.parameters.gamma = 1 # gas polytropic index (1.6666667) (default value:1.6666667)
     hydro_cloud.parameters.isothermal_flag = True # Isothermal flag. True means: isothermal gas (requires integrate_entropy_flag == False). (default value:False)
     hydro_cloud.parameters.integrate_entropy_flag = False # Integrate-entropy flag. True means: integrate entropy, else: internal energy. (default value:True)
-    hydro_cloud.parameters.timestep = dt # timestep for system (default value:4.70451599238e+13 s)
-
-    hydro_cloud.parameters.eps_is_h_flag = False  #Eps-is-h flag. True means: set gas particles gravitational epsilon to h (SPH smoothing length). (default value:True)
-                                                # h_smooth is constant
-    hydro_cloud.parameters.gas_epsilon = resolution # The gas gravitational smoothing epsilon.
-    hydro_cloud.parameters.sph_h_const = resolution # SPH smoothing length if constant
-    particles_cloud.h_smooth= resolution
-
-    hydro_cloud.gas_particles.add_particles(particles_cloud)
-
+       
     channel = {"hydro_to_part": hydro_cloud.gas_particles.new_channel_to(particles_cloud),
             "part_to_hydro": particles_cloud.new_channel_to(hydro_cloud.gas_particles)}
 
@@ -134,24 +141,27 @@ def evolve_molecular_cloud(particles_cloud, converter_cloud, t_end, dt, resoluti
 
     model_time = 0 | units.Myr
 
-    plot_hydro(model_time, hydro_cloud, L, N)
+    initial_colorbar = plot_hydro(model_time, hydro_cloud, L, N)
     print("ready for evolution")
 
     while model_time < t_end:
 
         model_time += dt
+        model_time = model_time.round(1)
+
         hydro_cloud.evolve_model(model_time)
         print("Time", model_time.in_(units.Myr))
         channel["hydro_to_part"].copy()
 
-    plot_hydro(model_time, hydro_cloud, L, N)
-    plot_hydro(model_time, hydro_cloud, 25, N)
+    final_colorbar = plot_hydro(model_time, hydro_cloud, L, N)
 
     hydro_cloud.stop()
 
-    return particles_cloud
+    print(f"Average mass of a SPH particle {particles_cloud.mass.sum().value_in(units.MSun)/len(particles_cloud.mass)}.")
+
+    return particles_cloud, final_colorbar
 
 # %%
 
 # particles_cloud, converter_cloud = make_molecular_cloud(100_000, 10_000_000 | units.MSun, 200 | units.parsec, 1312)
-# particles_cloud = evolve_molecular_cloud(particles_cloud, converter_cloud, 2 | units.Myr, 0.2 | units.Myr, 1 | units.RSun, 1312)
+# particles_cloud = evolve_molecular_cloud(particles_cloud, converter_cloud, 2 | units.Myr, 0.2 | units.Myr, 1312)
