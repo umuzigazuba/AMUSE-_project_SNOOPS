f3ee38c7121841e4f67d2cb14fb92b0981224394
luk7715 <eulrikagywu5@gmail.com>
2023-12-04 21:08:20 +0100

    added the functions to automate collition and accretion, and an example run file

---
+++ AMUSE_project_YESSIR/src/utils.py
@@ -0,0 +1,206 @@
+##This file contains functions that are hard coded for the purpose of colliding a 
+#globular cluster with a molecular cloud with our specific parameters 
+
+from molecular_cloud_initialization import *
+from plotters import *
+from cluster_cloud_initialization import *
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from amuse.community.fi.interface import Fi
+from amuse.lab import Particles, nbody_system
+from amuse.couple import bridge
+from amuse.units import units
+from amuse.community.bhtree.interface import Bhtree
+from amuse.ext.sink import new_sink_particles
+
+
+def hydro_code(Code, dt, converter, particles, seed):
+    '''
+    This function contains the parameters we want to initialise the 
+    hydro code with. (hard Coded)
+    '''
+    
+    np.random.seed(seed)
+
+    hydro = Code(converter)
+    hydro.parameters.use_hydro_flag = True # Hydrodynamics flag. True means:
+                            # SPH hydro included, False means: gravity only.
+    hydro.parameters.gamma = 1 # gas polytropic index (1.6666667)
+                        # (default value:1.6666667). In this case-> Ideal Gas   
+    hydro.parameters.timestep = dt
+    hydro.parameters.eps_is_h_flag = True # Default value
+    hydro.parameters.radiation_flag = False # turns off radiatiative cooling/heat.
+    hydro.parameters.isothermal_flag = True  # Isothermal flag. True means:
+                                            # isothermal gas (requires integrate_entropy_flag == False)
+    hydro.parameters.integrate_entropy_flag = False #True means: integrate
+                                                     # entropy, else: internal energy. 
+    hydro.gas_particles.add_particles(particles) # add the particles
+   
+    return hydro    
+
+def bondi_radius(stellar_mass):
+    sound_speed = 0.2 | units.kms
+    R = 2 * units.constants.G * stellar_mass /(sound_speed **2)
+
+    return R
+
+def bondi_accretion_rate(rho,v,r):
+    dM = units.constants.pi * (r**2) * rho * v
+
+    return dM
+
+
+def make_cluster_with_vinit(velocity,position,random_seed):
+    star_cluster = make_globular_cluster(star_count = 200,
+                                        imf = "kroupa", 
+                                        radius = 4 | units.pc,
+                                        metallicity = 0.002, 
+                                        age = 10 | units.Gyr, 
+                                        seed = random_seed)
+    
+    print("Most massive star in cluster is ", max(star_cluster.mass.in_(units.MSun)))
+
+    star_cluster.position +=  (-1.0, 0, 0) * (position | units.pc)
+    star_cluster.velocity += (1.0, 0, 0) * (velocity| units.kms)
+
+    converter_cluster = nbody_system.nbody_to_si(star_cluster.mass.sum(), 
+                                    star_cluster.position.sum())
+ 
+    return star_cluster,converter_cluster
+
+
+
+
+
+def AMUSE_bridge_initialization(star_cluster,converter_cluster,init_cloud,init_cloud_converter):
+    #initiate the gravity code with sink particles
+    gravity_code = Bhtree(converter_cluster)
+    sinks = new_sink_particles(star_cluster)
+
+    gravity_code.particles.add_particles(sinks)
+
+    # #start the hydro code for the gas
+    converter_cloud = init_cloud_converter
+    particles_cloud = init_cloud.copy()
+    hydro_cloud = hydro_code(Code = Fi, dt = 0.1 | units.Myr,
+                            converter = converter_cloud,
+                            particles = particles_cloud,
+                            seed = 1312)
+
+
+    channel = {"to_sinks": gravity_code.particles.new_channel_to(sinks),
+                "from_sinks": sinks.new_channel_to(gravity_code.particles),
+                "to_cloud": hydro_cloud.gas_particles.new_channel_to(particles_cloud),
+                "from_cloud": particles_cloud.new_channel_to(hydro_cloud.gas_particles)}
+
+
+    gravhydrobridge = bridge.Bridge(use_threading = False)
+    gravhydrobridge.add_system(gravity_code, (hydro_cloud,) )
+    gravhydrobridge.add_system(hydro_cloud, (gravity_code,) )
+    gravhydrobridge.timestep = 0.1 | units.Myr
+
+    return gravhydrobridge,sinks,channel,particles_cloud,gravity_code,hydro_cloud
+
+
+def let_them_collide(directory_path,t_end,dt,sinks,gravhydrobridge,\
+                     channel,particles_cloud,gravity_code,hydro_cloud,density_plot_flag=0):
+    t_end = t_end | units.Myr
+    model_time = 0 | units.Myr
+    dt = dt | units.Myr
+
+    sinks_mass_snapshots = []
+    current_velocity = sum(sinks.vx.value_in(units.kms))/len(sinks.vx)
+    print("Colliding with cluster velocity", current_velocity)
+
+    while model_time < t_end:
+        
+        # define the accreting radius of the sinks particle based on its Bondi radius
+        # IMPORTANT: the mass changes after each accretion event
+        sinks.sink_radius = bondi_radius(sinks.mass).in_(units.pc)
+
+        print("Largest sink radius", max(sinks.sink_radius).in_(units.pc))
+
+        print("Pre accretion cluster mass", sinks.mass.sum().in_(units.MSun))
+        
+        model_time += dt
+        model_time = model_time.round(1)
+        # evolve the gravity and hydro codes through our bridge
+        gravhydrobridge.evolve_model(model_time)
+
+
+        # update channels (copy over from the codes.particles to the particle sets)
+        channel["to_sinks"].copy()
+        channel["to_cloud"].copy()
+
+        print("Sinks in progress at", model_time.value_in(units.Myr), " Myr.")
+        # add the acretted mass to the sinks's total mass
+        sinks.accrete(particles_cloud)
+
+        # update channels (copy the information from the particle set to the gravity code)
+        channel["from_sinks"].copy()
+        channel["from_cloud"].copy()
+
+        # save the total mass of each step
+        sinks_mass_snapshots.append(sinks.mass.value_in(units.MSun))
+
+        print("Post accretion cluster mass", sinks.mass.sum().in_(units.MSun))
+        print(len(particles_cloud.mass), "number of cloud particles now")
+
+        if density_plot_flag == 1:
+                fig = plt.figure(figsize = (9, 5))
+                L=30
+                N=500
+                rho = make_map(hydro_cloud, L = L, N = N)
+                density_map = plt.imshow(np.log10(rho.value_in(units.amu/units.cm**3)), extent = [-L, L, -L, L])
+                plt.scatter(sinks.x.value_in(units.pc),sinks.y.value_in(units.pc),c='red')
+                color_bar = fig.colorbar(density_map)
+                color_bar.set_label('log density [$amu/cm^3$]', labelpad = 5)
+
+                plt.title(f"Molecular cloud at time = {model_time.value_in(units.Myr)} Myr and z = 0 pc")
+                plt.xlabel("x [pc]")
+                plt.ylabel("y [pc]")
+                plt.savefig(os.path.join(directory_path, f"density map \
+                                         at time_{model_time.value_in(units.Myr)}.png"))
+                plt.show()
+
+        
+
+
+        plt.scatter(particles_cloud.x.value_in(units.pc), particles_cloud.y.value_in(units.pc), s = 1)
+        plt.scatter(sinks.x.value_in(units.pc), sinks.y.value_in(units.pc), c = 'red', s = 5)
+        plt.title("Molecular cloud at time = " + model_time.as_string_in(units.Myr))
+        plt.xlabel("x [pc]")
+        plt.ylabel("y [pc]")
+        plt.savefig(os.path.join(directory_path, f"at time_{model_time.value_in(units.Myr)}.png"))
+        plt.close()
+
+
+    mass_difference = sinks_mass_snapshots[-1] - sinks_mass_snapshots[0]
+
+    # Save the list to a text file
+    with open(os.path.join(directory_path, \
+                           f"Sink mass_{current_velocity}.txt"), 'w') as file:
+        for sublist in sinks_mass_snapshots:
+            line = ' '.join(map(str, sublist))  # Convert sublist to a space-separated string
+            file.write(line + '\n')
+
+    print("Mass snapshots saved.")
+
+    plt.plot(sinks_mass_snapshots)
+    plt.xlabel("time [Myr]")
+    plt.ylabel("mass [Msun]")
+    plt.savefig(os.path.join(directory_path, f"Sink mass_{current_velocity}.png"))
+    plt.show()
+    plt.close()
+
+    plt.hist(mass_difference, bins  = 30)
+    plt.savefig(os.path.join(directory_path, f"Accreted mass hist_{current_velocity}.png"))
+    plt.close()
+
+    gravity_code.stop()
+    hydro_cloud.stop()
+    gravhydrobridge.stop()
+
+
