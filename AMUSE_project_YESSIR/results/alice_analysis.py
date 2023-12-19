cb87f36e9c7b8bf8e78c880f1c34532b281fd2c3
luk7715 <eulrikagywu5@gmail.com>
2023-12-19 14:26:49 +0100

    updated results analysis

--- AMUSE_project_YESSIR/results/alice_analysis.py
+++ AMUSE_project_YESSIR/results/alice_analysis.py
@@ -1,7 +1,12 @@
 #%%
 import os
+os.chdir('../src')
+
+import os
 import matplotlib.pyplot as plt
 import numpy as np
+from plotters import plot_evolution_mass_accretion, plot_relative_mass
+from amuse.units import units
 #%%
 
 
@@ -12,10 +17,11 @@ def extract_data_from_file(file_path):
     time = np.arange(1, len(data) + 1)
     return mass, time
 
+#%%
 
 
 # extract data from runs with different random seeds
-main_directory = "./alice/random_seeds"
+main_directory = "../results/alice/random_seeds"
 folders = os.listdir(main_directory)
 
 path_to_txt = []
@@ -28,13 +34,13 @@ for folder in folders:
             file_path = os.path.join(folder_path, file)
             all_path.append(file_path)
 
-all_path = np.reshape(all_path,(19,5))
+all_path = np.reshape(all_path,(19,10))
 
 #calculatating average and standard deviation for total accreted mass
 #with respect to each velocity
 total_accretion = []
 error = []
-for i in range(5):
+for i in range(10):
     mass_diff = []
     for j in range(19):
         mass,time = extract_data_from_file(all_path[j,i])
@@ -49,16 +55,40 @@ for i in range(5):
 #Plot result with errors 
 
 fig_path = os.path.join(main_directory,"total mass trend.png")
-v = np.array([20,30,40,50,60]) 
+v = np.array([20,25,30,35,40,45,50,55,60,65]) 
 
 plt.errorbar(v,total_accretion,yerr=error, fmt='o', \
              markersize=8, capsize=5, label='Total accretion with Error Bars')
 
-plt.xlabel('Cluster velocity [MSun]]')
+coefficients = np.polyfit(v, total_accretion, 1)
+poly_function = np.poly1d(coefficients)
+x_fit = np.linspace(min(v), max(v), 100)
+plt.plot(x_fit, poly_function(x_fit), color='red',linestyle = '--', label='Fitted Line')
+
+
+plt.xticks(v)
+plt.xlabel('Cluster velocity [km/s]')
 plt.ylabel('Total mass accreted [MSun]')
 plt.title('Total mass accretion against different velocities')
 plt.legend() 
 plt.grid(True)
 plt.savefig(fig_path)
 plt.show()
+# %%
+collision_velocity = 20
+file = '../results/alice/10000_stars/20 kms/Sink mass_20.00000000000005.txt'
+sinks_mass_evolution = np.loadtxt(file)
+sinks_mass_evolution = sinks_mass_evolution
+
+diff = sinks_mass_evolution[-1]- sinks_mass_evolution[0]
+
+plot_evolution_mass_accretion(sinks_mass_evolution = sinks_mass_evolution, 
+                                end_time = 2.8 | units.Myr, 
+                                time_step = 0.1 | units.Myr, 
+                                velocity = 20)
+
+plot_relative_mass(sinks_mass_evolution = sinks_mass_evolution, 
+                    velocity = 20
+                    )
+
 # %%
